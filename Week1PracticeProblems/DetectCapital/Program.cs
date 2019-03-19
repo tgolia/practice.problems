6dbdba169bfd873af00a85b538eefd32fe4b0073
JITTERBUG\travis.golia <travis.golia@greatcall.com>
2019-03-19 09:44:52 -0700

    1st 2 problems

---
+++ Week1PracticeProblems/DetectCapital/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace DetectCapital
+{
+    class Program
+    {
+        //Detect Capital
+        //==============
+        //Given a word, you need to judge whether the usage of capitals in it is right or
+        //not.
+
+        //We define the usage of capitals in a word to be right when one of the following
+        //cases holds:
+
+        //  1. All letters in this word are capitals, like "USA".
+        //  2. All letters in this word are not capitals, like "leetcode".
+        //  3. Only the first letter in this word is capital if it has more than one
+        //     letter, like "Google".
+
+        //Otherwise, we define that this word doesn't use capitals in a right way.
+
+        //Example 1:
+
+        //```
+        //Input: "USA"
+        //Output: True
+        //```
+
+        //Example 2:
+        //```
+        //Input: "FlaG"
+        //Output: False
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a word to check if it is has correct capitalization.");
+            string userEntry = Console.ReadLine();
+            while (userEntry != "exit")
+            {
+                Console.WriteLine(CheckCapitalization(userEntry));
+                Console.WriteLine("Enter another word to check or type \"exit\" to quit.");
+                userEntry = Console.ReadLine();
+            }
+        }
+
+        static Boolean CheckCapitalization(string word)
+        {
+            if (word.ToUpper() == word || word.ToLower() == word)
+            {
+                return true;
+            }
+            if (char.IsUpper(word[0]) && char.IsLower(word[1]))
+            {
+                for (int i = word.Length - 1; i > 0; i--)
+                {
+                    if (char.IsUpper(word[i]) == true)
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Week1PracticeProblems/ValidatePalindrome/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        //Validate Palindrome: 
+        //====================
+        //Given a string, determine if it is a palindrome, considering only alphanumeric
+        //characters and ignoring cases.
+
+        //Note: For the purpose of this problem, we define empty string as valid
+        //palindrome.
+
+        //Example 1:
+
+        //```
+        //Input: "A man, a plan, a canal: Panama"
+        //Output: true
+        //```
+
+        //Example 2:
+
+        //```
+        //Input: "race a car"
+        //Output: false
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string to check if it is a palindrome.");
+            string userEntry = RemoveNonalphanumericCharsAndConvertToLower(Console.ReadLine());
+            while (userEntry != "exit")
+            {
+                Console.WriteLine(IsPalindrome(userEntry));
+                Console.WriteLine("Enter another string to check or type \"exit\" to quit.");
+                userEntry = RemoveNonalphanumericCharsAndConvertToLower(Console.ReadLine());
+            }
+
+        }
+
+        public static string RemoveNonalphanumericCharsAndConvertToLower(string str)
+        {
+            Regex rgx = new Regex("[^a-zA-Z0-9]");
+            return rgx.Replace(str, "").ToLower();
+        }
+        public static Boolean IsPalindrome(string str)
+        {
+            if (ReverseString(str) == str)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static string ReverseString(string str)
+        {
+            string reversed = "";
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                reversed += str[i];
+            }
+            Console.WriteLine(reversed);
+            return reversed;
+        }
+    }
+}
